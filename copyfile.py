e68d362cc9f3eaf0254264c43b9fba7aa6d14e06
psychoCS <thiagoluizb@hotmail.com>
2020-08-04 12:44:22 -0400

    Properly commented the first task

--- copyfile.py
+++ copyfile.py
@@ -14,9 +14,12 @@ print()
 print("We will copy the contents of one file into another file")
 print()
 
+# Prompt the user for the first file 
 print("So please, enter first the name of the file that will have its contents transfered")
 firstFile = input()
 print()
+
+# Prompt the user for the second file
 print("Now enter the name of the file that will receive this information")
 secondFile = input()
 print()
@@ -26,20 +29,25 @@ print()
 #b. After the user enters the Destination file name the scripty will copy the contents of the Source file into the Destination file (6 Marks Functionality).
 class FileCopier():
 
+    # lets take the 2 user imputs 
     def copier(self,firstFile,secondFile):
         print()
 
+    # Open the first file and save its lines on a variable
     with open(firstFile,'r')as fileData:
         firstFileContent= fileData.readlines()
         print("Reading the content on the first file...")
         print()
+
+    # Open the second file and save its lines on a variable
     with open(secondFile,'r')as fileData:
         secondFileContent= fileData.readlines()
         print("Reading the content on the second file...")
         print()
 
+    # Pick each line of the second file and add it to the first one
     for eachline in firstFileContent:
         secondContent = open(secondFile,'a')
         secondContent.write(eachline)
-    print ("Done. Content from the first file transfered to the second one")
+    print("Done. Content from the first file transfered to the second one")
     print()
--- payroll.py
+++ payroll.py
@@ -10,7 +10,33 @@ Date: 04/08/2020
 #Task 2: The Payroll Department keeps a list of employee information for each pay period in a text file called data.txt. Write a script, payroll.py that reads the data.txt file and outputs the wages paid to each employee in tabular format to the console. (20 Marks: Functionality).
 
 
-#a. Create data.txt so that it creates 10 employee records. Each employee record should be comma separated and structured in the following way <Employee Number>,<LastName>,<FirstName>,<Hours Worked> (5 Marks: Functionality).
-
+#a. Create data.txt so that it creates 10 employee records. Each employee record should be comma separated and structured in the following way <Employee Number>,<LastName>,<FirstName>,<Hours Worked> (5 Marks: Functionality). - Done
 
 # b. Write payroll.py so that it reads data.txt and outputs each employee Record in a three column table nicely formatted with a Header Row that includes the following Employee Number | Employee Name | Hours Worked (15 Marks: Functionality)
+
+#step 1 is open a file
+fileObject = open("data.txt", "r")
+
+#step 2 - read from the file
+contents = fileObject.read()
+
+#step 3 - close out
+fileObject.close()
+
+
+print(contents)
+
+
+fileObject = open("numbers2.txt", "w+")
+numbers = [33, 55, 66, 77, 88, 99, 110, 121]
+
+count = 0
+
+for number in numbers:
+    count += 1
+    if count == len(numbers):
+        fileObject.write(str(number))
+    else:
+        fileObject.write(str(number) + ",")
+
+fileObject.close()
