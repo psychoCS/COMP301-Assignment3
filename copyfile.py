a72e30813f28c3adbbec2f8e588204cee29f3393
psychoCS <thiagoluizb@hotmail.com>
2020-08-04 11:30:01 -0400

    Starting

---
+++ copyfile.py
@@ -0,0 +1,10 @@
+''' 
+Name: copyfile.py
+Author: Thiago Batista
+Description: Assignment 3
+Program: COMP301
+Date: 04/08/2020
+'''
+
+#Task 1: Write a python console script named copyfile.py. This script will copy the contents of one file into another file
+
