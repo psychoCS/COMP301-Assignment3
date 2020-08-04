9ba426614b488222b6839d87aa60633abd32e948
psychoCS <thiagoluizb@hotmail.com>
2020-08-04 11:37:02 -0400

    created the prompts

--- copyfile.py
+++ copyfile.py
@@ -8,3 +8,16 @@ Date: 04/08/2020
 
 #Task 1: Write a python console script named copyfile.py. This script will copy the contents of one file into another file
 
+#a. The console script will prompt the user for two file names (Source and Destination) (4 Marks Functionality)
+print()
+print("We will copy the contents of one file into another file")
+print()
+
+print("So please, enter first the name of the file that will have its contents transfered")
+firstFile = input()
+print()
+print("Now enter the name of the file that will receive this information")
+secondFile = input()
+print()
+print("Ok. Lets transfer the content of the files")
+print()
